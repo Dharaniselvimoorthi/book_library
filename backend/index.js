53606fc74e64cbd5ae191a0b69b3e1f92aaf901f
Dharaniselvi <dharaniselvimoorthi@gmail.com>
2026-02-23 13:27:47 +0530

    be
    Signed-off-by: Dharaniselvi <dharaniselvimoorthi@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const Book = require("./model/Book");
+const Book = require("./model/book");
 
 const app = express();
 app.use(cors());
